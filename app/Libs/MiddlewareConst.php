9a4eef2bfe7b766c0023b814eac46877794b4a14
Vannyou TANG <vannyoutang23@gmail.com>
2019-03-21 19:18:56 +0700

    change method of getUser function from POST to GET

---
+++ app/Libs/MiddlewareConst.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vannyou.tan
+ * Date: 19-Mar-19
+ * Time: 7:15 PM
+ */
+
+namespace App\Libs;
+
+
+class MiddlewareConst
+{
+    const JWT_AUTH = 'jwt.auth';
+}
